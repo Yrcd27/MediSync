92505598d152b810920c171a54235f3643dd09f7
Yasiru <yasirucp2002@gmail.com>
2026-01-19 00:30:53 +0530

    remove unnecessary readme, directories and simplified the readme until I finalize it

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
-import '../core/constants/app_colors.dart';
 import '../core/constants/app_spacing.dart';
 import '../core/constants/app_typography.dart';
 
