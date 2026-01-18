4c78114ff996e1b20b28bec4b0b6a8619adcd148
Lakshan <lakshan.imantha02@gmail.com>
2026-01-18 15:57:05 +0530

    Add EnableAsync annotation to enable sending emails asynchronously

--- src/main/java/com/lakshan/medi_sync/MediSyncApplication.java
+++ src/main/java/com/lakshan/medi_sync/MediSyncApplication.java
@@ -2,12 +2,14 @@ package com.lakshan.medi_sync;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class MediSyncApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MediSyncApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MediSyncApplication.class, args);
+    }
 
 }
